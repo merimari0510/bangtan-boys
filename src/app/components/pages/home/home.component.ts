b7dca4e9b70498099d3fd120a6aa921b2b726eb2
merimari0510 <merimari051094@gmail.com>
2020-01-20 21:35:58 +0100

    Remove ngInit no utilize

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BtsService } from '../../../services/bts.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { BtsService } from '../../../services/bts.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   newAlbums: any[] = [];
   bts_info:any = {};
@@ -15,31 +15,21 @@ export class HomeComponent implements OnInit {
 
   constructor( private bts:BtsService ) {
     this.bts.getNewAlbums().subscribe( (data:any) => {
-      /* Obtengo la data, elimino el loading*/
-      console.log(data);
       this.newAlbums = data;
     });
     this.bts.getBTS().subscribe( (data:any) => {
-      console.log(data);
       this.bts_info = data;
     });
     this.bts.getMembers().subscribe( (data:any) => {
-      /* Obtengo la data, elimino el loading*/
-      console.log(data);
       this.members = data;
     });
     this.bts.getPositions().subscribe( (data:any) => {
-      /* Obtengo la data, elimino el loading*/
-      console.log(data);
       this.positions = data;
     });
   }
 
   showPosition(pos:number) {
     return this.positions[pos].name;
-  }
-
-  ngOnInit() {
   }
 
 }
